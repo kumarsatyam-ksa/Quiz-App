e9e362ef0d25f30ef8c9e125d92c1729937e42ec
kumarsatyam.ksa <kumarsatyam.ksa@gmail.com>
2024-07-23 22:14:53 +0530

    first commit

---
+++ quiz-questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        num: 1,
+        question: "What is the capital of Andhra Pradesh?",
+        answer: "Amaravati",
+        options: [
+            "Vishakhapatnam",
+            "Vijayawada",
+            "Tirupati",
+            "Amaravati"
+        ]
+    },
+    {
+        num: 2,
+        question: "What is the capital of Assam?",
+        answer: "Dispur",
+        options: [
+            "Guwahati",
+            "Silchar",
+            "Dispur",
+            "Dibrugarh"
+        ]
+    },
+    {
+        num: 3,
+        question: "What is the capital of Gujarat?",
+        answer: "Gandhinagar",
+        options: [
+            "Ahmedabad",
+            "Gandhinagar",
+            "Surat",
+            "Vadodara"
+        ]
+    },
+    {
+        num: 4,
+        question: "What is the capital of Haryana?",
+        answer: "Chandigarh",
+        options: [
+            "Chandigarh",
+            "Faridabad",
+            "Gurugram",
+            "Panipat"
+        ]
+    },
+    {
+        num: 5,
+        question: "What is the capital of Himachal Pradesh?",
+        answer: "Shimla",
+        options: [
+            "Dharamshala",
+            "Shimla",
+            "Manali",
+            "Dalhousie"
+        ]
+    },
+    {
+        num: 6,
+        question: "What is the capital of Kerala?",
+        answer: "Thiruvananthapuram",
+        options: [
+            "Kochi",
+            "Kozhikode",
+            "Thiruvananthapuram",
+            "Thrissur"
+        ]
+    },
+    {
+        num: 7,
+        question: "What is the capital of Meghalaya?",
+        answer: "Shillong",
+        options: [
+            "Baghmara",
+            "Cherrapunji",
+            "Tura",
+            "Shillong"
+        ]
+    },
+    {
+        num: 8,
+        question: "What is the capital of Nagaland?",
+        answer: "Kohima",
+        options: [
+            "Dimapur",
+            "Mokokchung",
+            "Kohima",
+            "Wokha"
+        ]
+    },
+    {
+        num: 9,
+        question: "What is the capital of Uttarakhand?",
+        answer: "Dehradun",
+        options: [
+            "Haridwar",
+            "Dehradun",
+            "Nainital",
+            "Rishikesh"
+        ]
+    },
+    {
+        num: 10,
+        question: "What is the capital of Uttar Pradesh?",
+        answer: "Lucknow",
+        options: [
+            "Lucknow",
+            "Kanpur",
+            "Varanasi",
+            "Prayagraj"
+        ]
+    }
+]
